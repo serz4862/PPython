32b6044bfe2b2f39697cce00ff92d3dca27ec6c6
serz4862 <sauravkumar4862@gmail.com>
2023-12-29 01:11:18 +0530

    qr generated

--- QR_code.py
+++ QR_code.py
@@ -1,10 +1,15 @@
 import qrcode
-from pyzbar.pyzbar import decode
+# from pyzbar.pyzbar import decode
 
-from PIL import Image
+# from PIL import Image
+features = qrcode.QRCode(version =1, box_size = 40,border = 4)
 
-LinkedIN = qrcode.make("https://www.linkedin.com/in/saurav-kumar-92bb521a8/")
-LinkedIN.save("linkedIn.png")
 
-d = decode(Image.open("linkedIn.png"))
-print(d[0].data.decode("ascii"))
+features.add_data("https://www.linkedin.com/in/saurav-kumar-92bb521a8/")
+features.make(fit= True)
+generate_image = features.make_image(fill_color = "blue",back_color = "yellow")
+generate_image.save("image.png")
+
+
+# d = decode(Image.open("linkedIn.png"))
+# print(d[0].data.decode("ascii"))
