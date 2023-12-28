a543baafc35b07e9b5234530d10a254ec142f8d4
serz4862 <sauravkumar4862@gmail.com>
2023-12-29 00:19:33 +0530

    QR decode

--- QR_code.py
+++ QR_code.py
@@ -1,5 +1,10 @@
 import qrcode
+from pyzbar.pyzbar import decode
+
+from PIL import Image
 
 LinkedIN = qrcode.make("https://www.linkedin.com/in/saurav-kumar-92bb521a8/")
 LinkedIN.save("linkedIn.png")
 
+d = decode(Image.open("linkedIn.png"))
+print(d[0].data.decode("ascii"))
